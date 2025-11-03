f613e547ea23fec7b690afbdf1e05aabc537c8ae
user-forty-seven <notlucky750@gmail.com>
2025-11-03 14:25:12 +0530

    cleanup: simplify error handling in main.cpp

--- main.cpp
+++ main.cpp
@@ -8,43 +8,20 @@
 #include <iostream>
 
 int main (int argc, char* argv[]) {
-  // std::string test = "    this     is               a                test";
-  // std::cout << "[BEFORE]: " << test << std::endl;
-  // utils::normalize_whitespaces(test);
-  // std::cout << "[AFTER]: " << test << std::endl;
-  // models::Time time = utils::get_current_time();
-  // std::cout << "[HOUR]: " << time.hour << std::endl;
-
   token::Command cmd = lexer::tokenize(argc, argv);
-  // utils::print_tokens(cmd);
   models::ParsedCommand parsed_cmd;
 
   try {
     parsed_cmd = parser::parse(cmd);
+    dispatcher::dispatch(parsed_cmd);
   }
   catch(const errors::ParseError& e)  {
     std::cerr << e.full_message() << std::endl;
     return 1;
   }
-  try {
-    dispatcher::dispatch(parsed_cmd);
-  }
-  catch (const errors::TaskError& e) {
+  catch(const errors::LoggrError& e)  { // catches TaskError, CommandError, DataError, ConfirmationError
     std::cerr << e.what() << std::endl;
     return 1;
   }
-  catch (const errors::CommandError& e) {
-    std::cerr << e.what() << std::endl;
-    return 1;
-  }
-  catch (const errors::ConfirmationError& e) {
-    std::cerr << e.what() << std::endl;
-    return 1;
-  }
-  catch (const errors::DataError& e) {
-    std::cerr << e.what() << std::endl;
-    return 1;
-  }
-  // loggr <command> [subcommand] [args...]
   return 0;
 } 
